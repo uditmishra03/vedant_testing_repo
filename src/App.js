205def566180365fd6f41d2fbdbdb3accda0b768
Jenkins CI <jenkins@yourdomain.com>
2025-03-09 19:29:51 +0530

    Update deployment image and App.js to uditmishra/react-app:2025-03-09_19-28-52

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
           <br />
           This soon be get deployed to our cluster...
         </a>
-        <p>Image: uditmishra/react-app:2025-03-09_19-22-18
+        <p>Image: uditmishra/react-app:2025-03-09_19-28-52
       </header>
     </div>
   );
